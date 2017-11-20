91d95edfc427ea4ccc26129427561b8fa4346b0e
Derek <dereklee@downtothis.com>
2017-11-21 08:47:29 +0900

    Initial Commit

---
+++ TestingFrameworksWrapper/TestingFrameworksWrapper.h
@@ -0,0 +1,19 @@
+//
+//  TestingFrameworksWrapper.h
+//  TestingFrameworksWrapper
+//
+//  Created by Derek Lee on 11/21/17.
+//  Copyright © 2017 Sun Rising App Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestingFrameworksWrapper.
+FOUNDATION_EXPORT double TestingFrameworksWrapperVersionNumber;
+
+//! Project version string for TestingFrameworksWrapper.
+FOUNDATION_EXPORT const unsigned char TestingFrameworksWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingFrameworksWrapper/PublicHeader.h>
+
+
